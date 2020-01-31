ace53c3e0cf9ac445ae34a87817f4c1b2be3d390
unknown <SherlockWang@itri.org.tw>
2020-01-31 18:15:42 +0800

    First step on discover devices

--- main.c
+++ main.c
@@ -2,10 +2,35 @@
 #include <stdlib.h>
 #include <libusb.h>
 
-int main()
-{
-    int init_state = libusb_init(NULL);
-    libusb_set_option(NULL, LIBUSB_OPTION_USE_USBDK);
+int void main(){
+    int init_success = libusb_init(NULL);
+    int libusb_success = libusb_set_option(NULL, LIBUSB_OPTION_USE_USBDK);
 
-    libusb_exit(NULL);
+    // discover devices
+    libusb_device **list;
+    libusb_device *found = NULL;
+    ssize_t cnt = libusb_get_device_list(NULL, &list);
+    ssize_t i = 0;
+    int err = 0;
+    if (cnt < 0)
+        error();
+
+    for (i = 0; i < cnt; i++) {
+        libusb_device *device = list[i];
+        if (is_interesting(device)) {
+            found = device;
+            break;
+        }
+    }
+
+    if (found) {
+        libusb_device_handle *handle;
+
+        err = libusb_open(found, &handle);
+        if (err)
+            error();
+        // etc
+    }
+
+    libusb_free_device_list(list, 1);
 }
--- 找工作/Linkedin/Two Awesome LinkedIn Summary Examples (with Templates) _ Blue Sky Resumes Blog_files/fpc.pl
+++
@@ -1,5 +0,0 @@
-// First Party Cookies
-// TS: 0 77810 dc5_ne1
-
-window.ITTs[0].FPCV = 'ZZi3d2Ur||';
-window.ITTs[0].setFPCookies();
