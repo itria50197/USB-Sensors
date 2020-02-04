48c96c7d7b04248472e8001d079590f34fd96ed2
unknown <SherlockWang@itri.org.tw>
2020-02-04 16:49:51 +0800

    Restart USB code

--- main.c
+++ main.c
@@ -2,35 +2,12 @@
 #include <stdlib.h>
 #include "libusb.h"
 
-void main(){
-    int init_success = libusb_init(NULL);
-    int libusb_success = libusb_set_option(NULL, LIBUSB_OPTION_USE_USBDK);
+void main()
+{
+    int return_init, return_setOption;
 
-    // discover devices
-    libusb_device **list;
-    libusb_device *found = NULL;
-    ssize_t cnt = libusb_get_device_list(NULL, &list);
-    ssize_t i = 0;
-    int err = 0;
-    if (cnt < 0)
-        error();
+    return_init = libusb_init(NULL);
+    //return_setOption = libusb_set_option(NULL,LIBUSB_LOG_LEVEL_WARNING);
 
-    for (i = 0; i < cnt; i++) {
-        libusb_device *device = list[i];
-        if (is_interesting(device)) {
-            found = device;
-            break;
-        }
-    }
-
-    if (found) {
-        libusb_device_handle *handle;
-
-        err = libusb_open(found, &handle);
-        if (err)
-            error();
-        // etc
-    }
-
-    libusb_free_device_list(list, 1);
+    //libusb_exit(NULL);
 }
