5a1401ebabd16e22ccdaf8029ab90010fc83e4d1
Sherlock Wang <sherlockwang@itri.org.tw>
2020-02-05 16:24:33 +0800

    Amendment on 20200205  16:20

--- discover devices.c
+++ discover devices.c
@@ -2,23 +2,33 @@
 #include <stdlib.h>
 #include "libusb.h"
 
+void error(){}
+int is_interesting(libusb_device *device){}
+
 void discover_devices(){
     // discover devices
     libusb_device **list;
     libusb_device *found = NULL;
-    ssize_t cnt = libusb_get_device_list(NULL, &list);
+    ssize_t num_detectedDevices = libusb_get_device_list(NULL, &list);
     ssize_t i = 0;
     int err = 0;
-    if (cnt < 0)
+    if (num_detectedDevices < 0)
+    {
         error();
-
-    for (i = 0; i < cnt; i++) {
-        libusb_device *device = list[i];
-        if (is_interesting(device)) {
-            found = device;
-            break;
+    }
+    else
+    {
+        for (i = 0; i < num_detectedDevices; i++) {
+            libusb_device *device = list[i];
+            if (is_interesting(device)) {
+                found = device;
+                break;
+            }
         }
     }
+
+
+
 
     if (found) {
         libusb_device_handle *handle;
@@ -31,3 +41,5 @@ void discover_devices(){
 
     libusb_free_device_list(list, 1);
 }
+
+
