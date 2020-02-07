366a2f793c65c05b3f8e37eea54158458bf0a8d5
Sherlock Wang <sherlockwang@itri.org.tw>
2020-02-07 17:44:31 +0800

    Update on 20200207  17:40

--- discover devices.c
+++ discover devices.c
@@ -3,12 +3,10 @@
 #include <windows.h>
 #include <libusb.h>
 
-void error(){printf("Detection Encounter Issues");}
-int interested_device(libusb_device *device){}
-
 void discover_devices(){ // discover devices
     int num_detectedDevices, i, err;
     libusb_device **list, *device;
+    libusb_device_handle *device_handle;
 
     num_detectedDevices = (int)libusb_get_device_list(NULL, &list); //ssize_t num_detectedDevices = libusb_get_device_list(NULL, &list);
     libusb_device *found[num_detectedDevices];
@@ -24,12 +22,10 @@ void discover_devices(){ // discover devices
         for (i = 0; i < num_detectedDevices; i++) {
             device = list[i];
             found[i] = device;
-            /*
-            if (interested_device(device)) {
-                found = device;
+            if (interested_device(device, device_handle)) {
+                found[i] = device;
                 break;
             }
-            */
         }
     }
 
@@ -42,7 +38,7 @@ void discover_devices(){ // discover devices
         // etc
     }
 
-    print_messages_discover(num_detectedDevices, err);
+    //libusb_close();
     libusb_free_device_list(list, 1);
 }
 
@@ -51,6 +47,12 @@ void print_messages_discover(ssize_t num_detectedDevices, int err){
     printf("num_detectedDevices = %d\n", num_detectedDevices);
     printf("err = %d\n", err);
     printf("End of discover_devices()\n");
+}
+
+void error(){printf("Detection Encounter Issues");}
+
+int interested_device(libusb_device *device, libusb_device_handle **device_handle){
+    libusb_open(device, &device_handle);
 }
 
 /* Wait to be added
